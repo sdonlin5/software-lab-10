eb6ed16bb169f4b5067f25f3ea9784c38c040480
Stephen Donlin <sddonlin@gmail.com>
2024-04-02 14:23:18 -0700

    db_create added to app.py

--- app.py
+++ app.py
@@ -7,9 +7,27 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Stephen Donlin in 3308'
 
-# Creates a route to the database
 @app.route('/db_test')
 def testing():
     conn = psycopg2.connect("postgres://render_db_52tl_user:qQfI0Iz8EuSisgvBnslUQWSBU0hUV2No@dpg-co66dt8l5elc73aclde0-a/render_db_52tl")
     conn.close()
     return "Database connection successful."
+
+@app.route('/db_create')
+def create():
+    conn = psycopg2.connect("postgres://render_db_52tl_user:qQfI0Iz8EuSisgvBnslUQWSBU0hUV2No@dpg-co66dt8l5elc73aclde0-a/render_db_52tl")
+    cur = conn.cursor()
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS Basketball(
+                First varchar(255), 
+                Last varchar(255), 
+                City varchar(255), 
+                Name varchar(255), 
+                Number int
+                );
+                ''')
+    conn.commit()
+    conn.close()
+    return "Basketball table created successfully."
+
+
